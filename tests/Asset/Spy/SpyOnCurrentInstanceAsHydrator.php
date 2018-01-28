0958aba2cf5e67880328e1c29cebf5d47e8c8e8c
Stratadox <jesse@stratadox.com>
2018-01-28 14:54:04 +0100

    Refactor hydrator spy to extend base class

--- tests/Asset/Spy/SpyOnCurrentInstanceAsHydrator.php
+++ tests/Asset/Spy/SpyOnCurrentInstanceAsHydrator.php
@@ -8,27 +8,8 @@ use PHPUnit\Framework\Assert;
 use ReflectionClass;
 use Stratadox\Hydration\Hydrates;
 
-class SpyOnCurrentInstanceAsHydrator implements Hydrates
+class SpyOnCurrentInstanceAsHydrator extends SpyOnCurrentInstance implements Hydrates
 {
-    private $expectedClass;
-    private $target;
-
-    public function __construct(string $expectedClass, ?Hydrates $target)
-    {
-        $this->expectedClass = $expectedClass;
-        $this->target = $target;
-    }
-
-    public static function expectThe(string $class, Hydrates $target = null)
-    {
-        return new self($class, $target);
-    }
-
-    public function onThe(Hydrates $target)
-    {
-        $this->target = $target;
-    }
-
     public function currentInstance()
     {
         return (new ReflectionClass($this->expectedClass))
